690c627c41abf054eeec73f615326a71a11ccb7c
gtoootg0131 <80195266+gtoootg0131@users.noreply.github.com>
2021-07-11 17:33:40 +0200

    Updated Header

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -3,16 +3,34 @@ import {Link} from 'react-router-dom'
 
 import styles from '../../styles/header.module.css'
 
-function Header(){
+function Header(props){
+    const{backgroundColor, color}=props
+
     return (
       // <div className="container">
-          <div className={styles.header}>
+          <div 
+            className={styles.header}
+            style={{
+                backgroundColor:backgroundColor,
+                color:color
+              }}
+            >
               <h className={styles.header__title}>Michihiro Goto`s Gallery</h>
               <div className={styles.header__navigation} >
-                <span><Link to ="/">Top</Link></span>
-                <span><Link to ="/profile">Profile</Link></span>
-                <span><a href="https://goto-travels.herokuapp.com/">Portfolio</a></span>         
-                <i className="header-logo fab fa-youtube"></i>
+                    <Link to ="/"　style={{color:color}}>
+                        <span>Top</span>
+                    </Link>
+                    <Link to ="/profile"　style={{color:color}}>
+                        <span>Profile</span>
+                    </Link>
+                    <span>
+                      <a 
+                        href="https://goto-travels.herokuapp.com/"
+                        style={{color:color}}
+                      >
+                      Portfolio
+                      </a>
+                    </span>     
                 <i className="header-logo fab fa-instagram"></i>
                 <i className="header-logo fab fa-linkedin"></i>
               </div>
