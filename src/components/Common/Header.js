6cdea08e923ec2cbc59a696e3e26ecb64a5a5438
gtoootg0131 <gotosan0131@yahoo.co.jp>
2022-07-02 10:25:09 +0200

    commit

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -1,64 +1,55 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
-import styles from '../../styles/header.module.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../../styles/header.module.css";
 
-function Header(props){
-    const{backgroundColor, color}=props
+function Header(props) {
+  const { backgroundColor, color } = props;
 
-    return (
-          <div 
-              className={styles.header}
-              style={{
-                  backgroundColor:backgroundColor,
-                  color:color
-                }}
+  return (
+    <div
+      className={styles.header}
+      style={{
+        backgroundColor: backgroundColor,
+        color: color,
+      }}
+    >
+      <h className={styles.header__title}>Michihiro Goto`s Gallery</h>
+      <div className={styles.header__navigation}>
+        <Link to="/" style={{ color: color }}>
+          <span>Top</span>
+        </Link>
+        <Link to="/profile" style={{ color: color }}>
+          <span>Profile</span>
+        </Link>
+        <span>
+          <a
+            href="https://goto-travels.herokuapp.com/"
+            style={{ color: color }}
           >
-              <h className={styles.header__title}>Michihiro Goto`s Gallery</h>
-              <div className={styles.header__navigation} >
-                    <Link to ="/"　style={{color:color}}>
-                        <span>Top</span>
-                    </Link>
-                    <Link to ="/profile"　style={{color:color}}>
-                        <span>Profile</span>
-                    </Link>
-                    <span>
-                        <a 
-                          href="https://goto-travels.herokuapp.com/"
-                          style={{color:color}}
-                        >
-                        Portfolio
-                        </a>
-                    </span>
-                    <a 
-                        href="https://github.com/gtoootg0131/Portfolio-Toppage2"
-                        style={{color:color}}
-                    > 
-                        <i 
-                            className="header-logo fab fa-github" 
-                            style={{color:color}}
-                        />
-                    </a>
-                    <a 
-                        href="https://www.instagram.com/gt.europa/"
-                        style={{color:color}}
-                    > 
-                        <i 
-                            className="header-logo fab fa-instagram" 
-                            style={{color:color}}
-                        />
-                    </a>
-                    <a 
-                        href="https://www.linkedin.com/in/%E5%80%AB%E5%AE%8F-%E5%BE%8C%E8%97%A4-37b905218/"
-                        style={{color:color}}
-                    >
-                        <i 
-                            className="header-logo fab fa-linkedin" 
-                            style={{color:color}}
-                        />
-                    </a>
-              </div>
-          </div>
-    );
+            Portfolio
+          </a>
+        </span>
+        <a
+          href="https://github.com/gtoootg0131/Portfolio-Toppage2"
+          style={{ color: color }}
+        >
+          <i className="header-logo fab fa-github" style={{ color: color }} />
+        </a>
+        <a style={{ color: color }}>
+          <i
+            className="header-logo fab fa-instagram"
+            style={{ color: color }}
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/%E5%80%AB%E5%AE%8F-%E5%BE%8C%E8%97%A4-37b905218/"
+          style={{ color: color }}
+        >
+          <i className="header-logo fab fa-linkedin" style={{ color: color }} />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
